eec051fa19f8df03221f2b4a7c755acc2eee215b
Tara Z. Manicsic <tara@modulus.io>
2016-01-12 23:45:45 -0500

    re-format app.js.

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const Express = require('express');
+const Path = require('path');
+const Favicon = require('serve-favicon');
+const Logger = require('morgan');
+const CookieParser = require('cookie-parser');
+const BodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+const Routes = require('./routes/index');
 
-var app = express();
+const App = Express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+App.set('views', Path.join(__dirname, 'views'));
+App.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+App.use(Logger('dev'));
+App.use(BodyParser.json());
+App.use(BodyParser.urlencoded({ extended: false }));
+App.use(CookieParser());
+App.use(Express.static(Path.join(__dirname, 'public')));
 
-app.use('/', routes);
+App.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+App.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -35,24 +35,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
-
 
 module.exports = app;
